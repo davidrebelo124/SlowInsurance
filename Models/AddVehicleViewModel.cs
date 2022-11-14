c817d3bbf54286e77da4ca9dd39d3a1eb2e9c85a
David Rebelo <david.m.rebelo@gmail.com>
2022-11-14 16:56:03 +0000

    Minor changes

---
+++ Models/AddVehicleViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SlowInsurance.Models
+{
+    public class AddVehicleViewModel
+    {
+        [Required]
+        [Display(Name = "Vehicle Type")]
+        public string? VehicleType { get; set; }
+        [Required]
+        public string? Model { get; set; }
+        [Required]
+        [Display(Name = "Registration Date")]
+        public string? RegistrationDate { get; set; }
+        [Required]
+        public string? Plate { get; set; }
+        [Required]
+        [Display(Name = "Adhesion Date")]
+        public string? AdhesionDate { get; set; }
+        [Required]
+        [Display(Name = "Payment Schedule")]
+        public string? PaymentSchedule { get; set; }
+    }
+}
