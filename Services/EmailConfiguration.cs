e63752d485c03d796327ab2c44740de1efd0a053
David Rebelo <david.m.rebelo@gmail.com>
2022-11-19 19:50:59 +0000

    Minor Changes

---
+++ Services/EmailConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace EmailService
+{
+    public class EmailConfiguration
+    {
+        public string? From { get; set; }
+        public string? SmtpServer { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; } = "very.slow.insurance@gmail.com";
+        public string Password { get; } = "fzpo cnfo bzur gjgq";
+    }
+}
