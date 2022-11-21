af4df1971117e48849ceb44dfbabb766e164f5f0
David Rebelo <david.m.rebelo@gmail.com>
2022-11-21 11:20:36 +0000

    Added a toggle to show password

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Toggle Password
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+
+//Toggle confirm password
+const toggleConfirmPassword = document.querySelector('#toggleConfirmPassword');
+const confirmpassword = document.querySelector('#confirmpassword');
+
+toggleConfirmPassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = confirmpassword.getAttribute('type') === 'password' ? 'text' : 'password';
+    confirmpassword.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
