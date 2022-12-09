e7206c7e9f8459b88624f182ec244d52db63ae12
David Rebelo <david.m.rebelo@gmail.com>
2022-12-09 17:57:38 +0000

    Minor Changes

--- Models/Vehicle/AddVehicleModel.cs
+++ Models/Vehicle/AddVehicleModel.cs
@@ -5,20 +5,22 @@ namespace SlowInsurance.Models.Vehicle
     public class AddVehicleModel
     {
         [Required]
+        [MaxLength(100)]
         [Display(Name = "Vehicle Type")]
         public string? VehicleType { get; set; }
         [Required]
+        [MaxLength(500)]
         public string? Model { get; set; }
         [Required]
+        [DataType(DataType.Date)]
         [Display(Name = "Registration Date")]
         public string? RegistrationDate { get; set; }
         [Required]
+        [RegularExpression("/^(([A-Z]{2}-\\d{2}-(\\d{2}|[A-Z]{2}))|(\\d{2}-(\\d{2}-[A-Z]{2}|[A-Z]{2}-\\d{2})))$/")]
         public string? Plate { get; set; }
         [Required]
+        [DataType(DataType.Date)]
         [Display(Name = "Adhesion Date")]
         public string? AdhesionDate { get; set; }
-        [Required]
-        [Display(Name = "Payment Schedule")]
-        public string? PaymentSchedule { get; set; }
     }
 }
