dbc829593e724c8c89a5e14bc38541736e89820d
David Rebelo <david.m.rebelo@gmail.com>
2022-12-18 00:26:07 +0000

    Minor Changes

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -57,7 +57,7 @@ namespace EmailService
         private MimeMessage CreateEmailMessage(Message message)
         {
             var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress("email", _emailConfig.From));
+            emailMessage.From.Add(new MailboxAddress("Slow Insurance", _emailConfig.From));
             emailMessage.To.AddRange(message.To);
             emailMessage.Subject = message.Subject;
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = message.Content };
